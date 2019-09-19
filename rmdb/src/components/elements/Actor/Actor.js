d0047110a3b5bce8ce633aaaa92847e6765d0bef
Chris Jackson <chrisjackson@admins-MacBook-Pro.local>
2019-09-19 12:13:26 -0400

    semi finished product

---
+++ rmdb/src/components/elements/Actor/Actor.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../../../config';
+import './Actor.css';
+
+const Acter = (props) => {
+
+    const POSER_SIZE = "w154";
+
+    return (
+        <div className="rmdb-actor">
+            <img
+                src={props.actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.actor.profile_path}` : './images/no_image.jpg'}
+            />
+            <span className="rmdb-actor-name">{props.actor.name}</span>
+            <span className="rmdb-actor-character">{props.actor.character}</span>
+        </div>
+    )
+}
+
+export default Acter;
